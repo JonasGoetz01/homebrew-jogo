b44bdbfd7f031adc12e28360b42a1cfd3b124ffa
Jonas Götz <jonas.goetz@egotec.com>
2023-04-01 14:35:24 +0200

    testscript

---
+++ testscript.rb
@@ -0,0 +1,11 @@
+class Testscript < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/JonasGoetz01/brew-scripts/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "2a7d0aa6259239a37485ac1cb7524fab6fb026e7788c1c8e0db9e97e35b2c559"
+  license ""
+
+  def install
+    bin.install "testscript"
+  end
+end
