f9342b657e7b8f17ba87684487c4de460fbfd227
Jonas Götz <jonas.goetz@egotec.com>
2023-04-01 23:40:37 +0200

    automated commit by makeTag.sh

--- log.rb
+++ log.rb
@@ -1,8 +1,8 @@
 class Log < Formula
   desc ""
   homepage ""
-  url "https://github.com/JonasGoetz01/brew-scripts/archive/refs/tags/v1.0.18.tar.gz"
-  sha256 "cc33dfe92bb7e130130ae6540f288060ee9658300f59849a54c62096bdda09f0"
+  url "https://github.com/JonasGoetz01/brew-scripts/archive/refs/tags/v1.0.19.tar.gz"
+  sha256 "79b4eca00b2781922eebc0c8e5a304748e4011ec41b9fb0a75f29c526aeb09f8"
   license ""
   def install
     bin.install "log"
