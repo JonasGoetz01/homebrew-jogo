63976434b8ad36a3b5e293ff2c2fce6f994f611a
Jonas Götz <jonas.goetz@egotec.com>
2023-04-02 00:28:20 +0200

    automated commit by makeTag.sh

--- gpg-file.rb
+++ gpg-file.rb
@@ -1,10 +1,10 @@
-class Update < Formula
+class GpgFile < Formula
   desc ""
   homepage ""
   url "https://github.com/JonasGoetz01/brew-scripts/archive/refs/tags/v1.0.20.tar.gz"
   sha256 "07189c98cfee9f8812fba58adce53e447f30dfe3f25ceaf072c9ee3516352f18"
   license ""
   def install
-    bin.install "update"
+    bin.install "gpg-file"
   end
 end
--- log.rb
+++ log.rb
@@ -1,8 +1,8 @@
 class Log < Formula
   desc ""
   homepage ""
-  url "https://github.com/JonasGoetz01/brew-scripts/archive/refs/tags/v1.0.19.tar.gz"
-  sha256 "79b4eca00b2781922eebc0c8e5a304748e4011ec41b9fb0a75f29c526aeb09f8"
+  url "https://github.com/JonasGoetz01/brew-scripts/archive/refs/tags/v1.0.20.tar.gz"
+  sha256 "07189c98cfee9f8812fba58adce53e447f30dfe3f25ceaf072c9ee3516352f18"
   license ""
   def install
     bin.install "log"
