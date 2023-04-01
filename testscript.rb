39cea1c6a1ff22cc8fcc699dd8572a4bd2990b80
Jonas Götz <jonas.goetz@egotec.com>
2023-04-01 16:33:39 +0200

    automated commit by makeTag.sh

--- testscript.rb
+++ testscript.rb
@@ -1,10 +1,13 @@
+==> Downloading https://github.com/JonasGoetz01/brew-scripts/archive/refs/tags/v1.0.13.tar.gz
+Already downloaded: /Users/jogo/Library/Caches/Homebrew/downloads/b8f0eb8f2688832f91ae2876fb46aec6a82ab0d76a95e838837a5b3d4e4026ec--brew-scripts-1.0.13.tar.gz
+Please run `brew audit --new testscript` before submitting, thanks.
+Editing /opt/homebrew/Library/Taps/homebrew/homebrew-core/Formula/testscript.rb
 class Testscript < Formula
   desc ""
   homepage ""
-  url "https://github.com/JonasGoetz01/brew-scripts/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "2a7d0aa6259239a37485ac1cb7524fab6fb026e7788c1c8e0db9e97e35b2c559"
+  url "https://github.com/JonasGoetz01/brew-scripts/archive/refs/tags/v1.0.13.tar.gz"
+  sha256 "43e94bf4d282ff3a07bd6b52cb903bc04850f2d03e37b57a80482bb9ccc353d7"
   license ""
-
   def install
     bin.install "testscript"
   end
--- testscript2.rb
+++ testscript2.rb
@@ -1,12 +1,14 @@
+==> Downloading https://github.com/JonasGoetz01/brew-scripts/archive/refs/tags/v1.0.13.tar.gz
+Already downloaded: /Users/jogo/Library/Caches/Homebrew/downloads/b8f0eb8f2688832f91ae2876fb46aec6a82ab0d76a95e838837a5b3d4e4026ec--brew-scripts-1.0.13.tar.gz
+Please run `brew audit --new testscript2` before submitting, thanks.
+Editing /opt/homebrew/Library/Taps/homebrew/homebrew-core/Formula/testscript2.rb
 class Testscript2 < Formula
   desc ""
   homepage ""
-  url "https://github.com/JonasGoetz01/brew-scripts/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "3b0cd29301dd16b9e155b5aac07c6b6140cec8bf8dd2531140b5813c80393af7"
+  url "https://github.com/JonasGoetz01/brew-scripts/archive/refs/tags/v1.0.13.tar.gz"
+  sha256 "43e94bf4d282ff3a07bd6b52cb903bc04850f2d03e37b57a80482bb9ccc353d7"
   license ""
-
   def install
     bin.install "testscript2"
   end
-
 end
