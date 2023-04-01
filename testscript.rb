a8bfbf8f488f9aad608240a46f5618bd47782885
Jonas Götz <jonas.goetz@egotec.com>
2023-04-01 16:49:13 +0200

    automated commit by makeTag.sh

--- testscript.rb
+++ testscript.rb
@@ -1,8 +1,8 @@
 class Testscript < Formula
   desc ""
   homepage ""
-  url "https://github.com/JonasGoetz01/brew-scripts/archive/refs/tags/v1.0.13.tar.gz"
-  sha256 "43e94bf4d282ff3a07bd6b52cb903bc04850f2d03e37b57a80482bb9ccc353d7"
+  url "https://github.com/JonasGoetz01/brew-scripts/archive/refs/tags/v1.0.14.tar.gz"
+  sha256 "541186dfd6dde9b37b1b9973854cfe0fda019a738783fd41a66d8dcf85ec13a0"
   license ""
   def install
     bin.install "testscript"
