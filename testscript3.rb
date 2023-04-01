0bd708b880283a300cf6d0f1a00d6ae8fc21d1ea
Jonas Götz <jonas.goetz@egotec.com>
2023-04-01 16:38:48 +0200

    automated commit by makeTag.sh

---
+++ testscript3.rb
@@ -0,0 +1,14 @@
+==> Downloading https://github.com/JonasGoetz01/brew-scripts/archive/refs/tags/v1.0.13.tar.gz
+Already downloaded: /Users/jogo/Library/Caches/Homebrew/downloads/b8f0eb8f2688832f91ae2876fb46aec6a82ab0d76a95e838837a5b3d4e4026ec--brew-scripts-1.0.13.tar.gz
+Please run `brew audit --new testscript3` before submitting, thanks.
+Editing /opt/homebrew/Library/Taps/homebrew/homebrew-core/Formula/testscript3.rb
+class Testscript3 < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/JonasGoetz01/brew-scripts/archive/refs/tags/v1.0.13.tar.gz"
+  sha256 "43e94bf4d282ff3a07bd6b52cb903bc04850f2d03e37b57a80482bb9ccc353d7"
+  license ""
+  def install
+    bin.install "testscript3"
+  end
+end
