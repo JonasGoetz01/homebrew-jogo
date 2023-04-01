1cf31f9b9c22cc5b823a012a1b5a1315f29e6c5f
Jonas Götz <jonas.goetz@egotec.com>
2023-04-01 15:03:59 +0200

    neue formula

---
+++ testscript2.rb
@@ -0,0 +1,12 @@
+class Testscript2 < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/JonasGoetz01/brew-scripts/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "3b0cd29301dd16b9e155b5aac07c6b6140cec8bf8dd2531140b5813c80393af7"
+  license ""
+
+  def install
+    bin.install "testscript2"
+  end
+
+end
